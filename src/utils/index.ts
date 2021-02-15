0a806a8694629863d3ec417df6c51ee96111aa17
Takeshi Iwana <iwatakeshi@gmail.com>
2021-02-14 22:13:25 -0700

    Fix typo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export { default as scale } from './scale';
 export * from './translate';
-export { default as mergeClassname } from './merge-classname';
+export { default as mergeClassName } from './merge-classname';
 export { default as getRef } from './get-ref';
